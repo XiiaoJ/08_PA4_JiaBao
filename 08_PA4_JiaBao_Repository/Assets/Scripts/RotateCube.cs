a842acb8fa399bfb732b50fac467e1df7011cf12
XiiaoJ <64391002+XiiaoJ@users.noreply.github.com>
2020-05-04 16:39:27 +0800

    Unity project updated
    Added a unity project

---
+++ 08_PA4_JiaBao_Repository/Assets/Scripts/RotateCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public Vector3 rotateSphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateSphere * Time.deltaTime);
+    }
+}
